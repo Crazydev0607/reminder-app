67629a7554fdbfcbab671899d925763f86c49f2a
Crazydev0607 <crazydev0607@gmail.com>
2025-03-18 19:22:23 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    console.log('Service Worker Installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker Activated');
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    clients.openWindow('/');
+});
